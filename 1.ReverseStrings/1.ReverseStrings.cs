1e82be577fed7ea7c6a7b2ef624002dec5696307
StanM84 <stan1984@abv.bg>
2023-11-07 16:39:57 +0200

    Add project files.

---
+++ 1.ReverseStrings/1.ReverseStrings.cs
@@ -0,0 +1,21 @@
+﻿string word = Console.ReadLine();
+
+while (word != "end")
+{
+    //string reversed = "";
+
+    //for (int i = word.Length - 1 ; i >= 0; i -= 1 )
+    //{
+    //    reversed += word[i];
+    //}
+
+    //char[] wordAsCharArray = word.ToCharArray();
+    //Array.Reverse(wordAsCharArray);
+    //string reversed = string.Join("", wordAsCharArray);
+
+    string reversed = string.Join("", word.ToCharArray().Reverse());
+
+    Console.WriteLine($"{word} = {reversed}");
+
+    word = Console.ReadLine();
+}
---
+++ 4.TextFilter/5.TextFilter.cs
@@ -0,0 +1,16 @@
+﻿string[] bannedWords = Console.ReadLine().Split(", ");
+
+string text = Console.ReadLine();
+
+foreach (string word in bannedWords)
+{
+    //string censorWord = "".PadLeft(word.Length, '*');
+    string censorWord = new string('*', word.Length);
+
+    while (text.Contains(word))
+    {
+        text = text.Replace(word, censorWord);
+    }
+}
+
+Console.WriteLine(text);
